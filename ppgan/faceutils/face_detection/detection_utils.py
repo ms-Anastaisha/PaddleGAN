a7ad04af8e66de111e536734cc9dfda2d46a6ba1
Victor Penzurov <vpenzurov@yandex.ru>
2021-11-15 15:47:56 +0300

    change detection postporocessing

--- ppgan/faceutils/face_detection/detection_utils.py
+++ ppgan/faceutils/face_detection/detection_utils.py
@@ -87,7 +87,7 @@ def upscale_detection(detection, max_coords):
     bh, bw = detection[3] - detection[1], detection[2] - detection[0]
     min_x1, min_y1, max_x2, max_y2 = max_coords
     ratio_y1, ratio_y2 = 0.95 + bh / max_y2 / 2, 0.65 + bh / max_y2 / 2
-    ratio_x = 0.6 + bw / max_x2 / 2  
+    ratio_x = 0.95 + bw / max_x2 / 2  
     cy, cx = detection[1] + int(bh / 2), detection[0] + int(bw / 2)
     y1, x1 = max(min_y1, cy - int(bh * ratio_y1)), max(min_x1, cx - int(bw * ratio_x))
     y2, x2 = min(max_y2, cy + int(bh * ratio_y2)), min(max_x2, cx + int(bw * ratio_x))
@@ -192,3 +192,40 @@ def polygon2ellipsemask(polygon, shape):
                             angle, 0, 360, color=255, thickness=-1)
     
 
+def compute_increased_bbox(bbox, frame_shape, increase_area):
+        left, top, right, bot = bbox
+        width = right - left
+        height = bot - top
+    
+        left = int(left - increase_area * width)
+        top = int(top - increase_area * height)
+        right = int(right + increase_area * width)
+        bot = int(bot + increase_area * height)
+
+        left = np.clip(left, 0, frame_shape[1])
+        right = np.clip(right, 0, frame_shape[1])
+        top = np.clip(top, 0, frame_shape[0])
+        bot = np.clip(bot, 0, frame_shape[0])
+
+        return [left, top, right, bot]
+
+
+def compute_aspect_preserved_bbox(bbox, frame_shape, increase_area):
+    left, top, right, bot = bbox
+    width = right - left
+    height = bot - top
+ 
+    width_increase = max(increase_area, ((1 + 2 * increase_area) * height - width) / (2 * width))
+    height_increase = max(increase_area, ((1 + 2 * increase_area) * width - height) / (2 * height))
+
+    left = int(left - width_increase * width)
+    top = int(top - height_increase * height)
+    right = int(right + width_increase * width)
+    bot = int(bot + height_increase * height)
+
+    left = np.clip(left, 0, frame_shape[1])
+    right = np.clip(right, 0, frame_shape[1])
+    top = np.clip(top, 0, frame_shape[0])
+    bot = np.clip(bot, 0, frame_shape[0])
+
+    return (left, top, right, bot)
