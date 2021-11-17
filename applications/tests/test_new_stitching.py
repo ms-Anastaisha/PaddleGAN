0e3a38873d7a7facad22e637d19da6ab400dd052
ms-Anastaisha <ms.urmanana@gmail.com>
2021-11-17 10:36:12 +0300

    update test_new_stitching

--- applications/tests/test_new_stitching.py
+++ applications/tests/test_new_stitching.py
@@ -1,6 +1,9 @@
 import sys
-sys.path.insert(0, '/home/anastasia/paddleGan/PaddleGAN/')
 import os 
+
+cur_path = os.path.abspath(os.path.dirname(__file__))
+root_path = os.path.split(cur_path)[0]
+sys.path.insert(0, os.path.dirname(root_path))
 from ppgan.apps.first_order_predictor import FirstOrderPredictor
 import time
 import pathlib
@@ -21,22 +24,22 @@ args = {
     "gfpgan_model_path": None, #"/home/user/paddle/PaddleGAN/experiments/pretrained_models/GFPGANCleanv1-NoCE-C2.pth",
     "mobile_net": False,
     "preprocessing": True,
-    "solov_path": "/home/anastasia/paddleGan/PaddleGAN/PaddleDetection/solov2_r50_enhance_coco"
+    "solov_path": "/home/user/paddle/PaddleGAN/PaddleDetection/solov2_r50_enhance_coco"
 }
 resources = {
-    "source_image": "/home/anastasia/paddleGan/PaddleGAN/data/source_image",
+    "source_image": "/home/user/paddle/PaddleGAN/data/source_image",
                     #[#"/home/user/paddle/PaddleGAN/data/selfie.jpeg", 
                     #"/home/anastasia/paddleGan/PaddleGAN/data/source_image/download-21.png"],
                     #"/home/user/paddle/PaddleGAN/data/selfie4.jpg", 
                     #"/home/user/paddle/PaddleGAN/data/selfie5.jpg"],
-    "driving_video": ["/home/anastasia/paddleGan/PaddleGAN/data/video16.mp4",
-    # "/home/anastasia/paddleGan/PaddleGAN/data/mayiyahei.MP4", 
-    # "/home/anastasia/paddleGan/PaddleGAN/data/jingle_bells_acapella_part.mp4"
+    "driving_video": ["/home/user/paddle/PaddleGAN/data/Remini_Animation (1).mp4",
+    "/home/user/paddle/PaddleGAN/data/video.mp4", 
+    "/home/user/paddle/PaddleGAN/data/jingle_bells_acapella_part.mp4"
     ], 
     "audio": None, 
-    "borders": {"landscape": "/home/anastasia/paddleGan/PaddleGAN/data/borders/landscape_border.png",
-                "square": "/home/anastasia/paddleGan/PaddleGAN/data/borders/square_border.png", 
-                "portrait": "/home/anastasia/paddleGan/PaddleGAN/data/borders/portrait_border.png"}
+    "borders": {"landscape": "/home/user/paddle/PaddleGAN/data/borders/landscape_border.png",
+                "square": "/home/user/paddle/PaddleGAN/data/borders/square_border.png", 
+                "portrait": "/home/user/paddle/PaddleGAN/data/borders/portrait_border.png"}
 }
 if __name__ == '__main__':
     start = time.time()
