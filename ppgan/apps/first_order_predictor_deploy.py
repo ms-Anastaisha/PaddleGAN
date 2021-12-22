f26e7e68cd732c2ae3d203bf9945796967315e5e
Alexey Ushakov <kafka.pochta@gmail.com>
2021-12-23 00:44:11 +0300

    typo

--- ppgan/apps/first_order_predictor_deploy.py
+++ ppgan/apps/first_order_predictor_deploy.py
@@ -455,7 +455,7 @@ class FirstOrderPredictor(BasePredictor):
             # os.remove(temp.name)
             videoclip_2 = mp.ImageSequenceClip(out_frame, fps=fps)
             videoclip_2.write_videofile(
-                out_file.name, preset="ultrafast", audio=audio, audio_codec="aac", threads=2Z, logger=None
+                out_file.name, preset="ultrafast", audio=audio, audio_codec="aac", threads=2, logger=None
             )
             videoclip_2.close()
             print("Audio time: {}".format(time() - s2))
